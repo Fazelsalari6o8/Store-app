ab01279ee121f312e1a89ce7c6e9eb7f864d4c13
Anonymous Soldier <a96111085154015@gmail.com>
2024-01-29 16:39:53 +0330

    Creating a custom hook to get product data from the context

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,20 @@ import DetailsPage from "./pages/DetailsPage.jsx";
 import CheckoutPage from "./pages/CheckoutPage.jsx";
 import PageNotFound from "./pages/404.jsx";
 
+// context provider
+import ProductsProvider from "./context/ProductContext.jsx";
+
 function App() {
   return (
-    <Routes>
-      <Route index element={<Navigate to="/products" />} />
-      <Route path="/products" element={<ProductsPage />} />
-      <Route path="/products/:id" element={<DetailsPage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-      <Route path="/*" element={<PageNotFound />} />
-    </Routes>
+    <ProductsProvider>
+      <Routes>
+        <Route index element={<Navigate to="/products" />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<DetailsPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </ProductsProvider>
   );
 }
 
