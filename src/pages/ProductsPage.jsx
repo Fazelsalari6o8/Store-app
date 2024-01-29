3f13350fca0c1b9329476f1df0bd6b07340cc58b
Anonymous Soldier <a96111085154015@gmail.com>
2024-01-29 17:32:42 +0330

    Render the product list

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,12 +1,22 @@
+// hooks
 import { useProducts } from "../context/ProductContext";
+
+// styles
+import styles from "./ProductsPage.module.css";
 
 function ProductsPage() {
   const products = useProducts();
   console.log(products);
   return (
-    <>
-      <h1>SalarShop</h1>
-    </>
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>loading...</p>}
+        {products.map((product) => (
+          <p key={product.id}>{product.title}</p>
+        ))}
+      </div>
+      <div>Sidebar</div>
+    </div>
   );
 }
 
