0d9976be2a0736f04f11a03a028faf1c0c58c043
Anonymous Soldier <a96111085154015@gmail.com>
2024-01-29 15:20:57 +0330

    Create routing for main pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+// library
+import { Navigate, Route, Routes } from "react-router-dom";
+
+// pages
+import ProductsPage from "./pages/ProductsPage.jsx";
+import DetailsPage from "./pages/DetailsPage.jsx";
+import CheckoutPage from "./pages/CheckoutPage.jsx";
+import PageNotFound from "./pages/404.jsx";
+
 function App() {
   return (
-    <>
-      <h1>SalarShop</h1>
-    </>
+    <Routes>
+      <Route index element={<Navigate to="/products" />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<DetailsPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
   );
 }
 
