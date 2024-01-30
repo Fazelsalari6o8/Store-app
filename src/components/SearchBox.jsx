166db9da2616214f73c3c270f718e417c77e7310
Anonymous Soldier <a96111085154015@gmail.com>
2024-01-30 23:27:31 +0330

    Styling the search section and sidebar

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -5,13 +5,16 @@ import { ImSearch } from "react-icons/im";
 // helper
 import { createQueryObject } from "../helper/helper.js";
 
+// styles
+import styles from "./SearchBox.module.css";
+
 function SearchBox({ search, setSearch, setQuery }) {
   const searchHandler = () => {
     setQuery((query) => createQueryObject(query, { search: search.trim() }));
   };
 
   return (
-    <div>
+    <div className={styles.search}>
       <input
         type="text"
         placeholder="Search..."
