d9114ecb8749568f9da95fe5ebecfd3dccb8d6bd
Anonymous Soldier <a96111085154015@gmail.com>
2024-02-02 23:31:07 +0330

    Implement checkout page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,5 +1,33 @@
+// library
+// custom hook
+import { useCart } from "../context/CartContext.jsx";
+
+// components
+import BasketCard from "../components/BasketCard.jsx";
+
+// styles
+import styles from "./CheckoutPage.module.css";
+
 function CheckoutPage() {
-  return <div>CheckoutPage</div>;
+  const [state, dispatch] = useCart();
+
+  const clickHandler = (type, payload) => {
+    dispatch({ type, payload });
+  };
+
+  return (
+    <div className={styles.checkout}>
+      <div>
+        {state.selectedItems.map((product) => (
+          <BasketCard
+            key={product.id}
+            data={product}
+            clickHandler={clickHandler}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CheckoutPage;
