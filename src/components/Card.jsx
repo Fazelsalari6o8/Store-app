a683d0b1e37e2153af506904c512aede01f1ddc4
Anonymous Soldier <a96111085154015@gmail.com>
2024-02-02 15:58:17 +0330

    Handle buttons based on the number of products

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,15 @@
 // library
-import { Link } from "react-router-dom";
-// hooks
+// custom hooks
 import { useCart } from "../context/CartContext.jsx";
+// router
+import { Link } from "react-router-dom";
 // icons
 import { TbListDetails } from "react-icons/tb";
 import { TbShoppingBagCheck } from "react-icons/tb";
+import { MdDeleteOutline } from "react-icons/md";
 
 // helper
-import { shortenText } from "../helper/helper";
+import { productQuantity, shortenText } from "../helper/helper";
 
 // styles
 import styles from "./Card.module.css";
@@ -16,10 +18,10 @@ function Card({ data }) {
   const { id, title, image, price } = data;
 
   const [state, dispatch] = useCart();
-  console.log(state);
+  const quantity = productQuantity(state, id);
 
-  const clickHandler = () => {
-    dispatch({ type: "ADD_ITEM", payload: data });
+  const clickHandler = (type) => {
+    dispatch({ type, payload: data });
   };
 
   return (
@@ -32,9 +34,23 @@ function Card({ data }) {
           <TbListDetails />
         </Link>
         <div>
-          <button onClick={clickHandler}>
-            <TbShoppingBagCheck />
-          </button>
+          {quantity === 1 ? (
+            <button onClick={() => clickHandler("REMOVE_ITEM")}>
+              <MdDeleteOutline />
+            </button>
+          ) : (
+            quantity > 1 && (
+              <button onClick={() => clickHandler("DECREASE")}>-</button>
+            )
+          )}
+          {!!quantity && <span>{quantity}</span>}
+          {quantity === 0 ? (
+            <button onClick={() => clickHandler("ADD_ITEM")}>
+              <TbShoppingBagCheck />
+            </button>
+          ) : (
+            <button onClick={() => clickHandler("INCREASE")}>+</button>
+          )}
         </div>
       </div>
     </div>
